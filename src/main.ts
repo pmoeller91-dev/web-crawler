757ac0429c59d98b818abfc4dbffce471606704d
pmoeller91 <pmoeller91@gmail.com>
2024-07-08 15:27:43 -0500

    feat: add basic command-line parsing

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log("hello world");
+import { Command } from "commander";
+
+const version = "0.1.0";
+
+const webCrawler = new Command();
+
+webCrawler
+  .name("crawl")
+  .description("CLI utility to extract links from a target URL")
+  .version(version)
+  .argument("<url>", "Base URL to crawl for links")
+  .showHelpAfterError()
+  .action((url: string) => {
+    console.log(`Crawler is starting using a base URL of "${url}"...`);
+  });
+
+webCrawler.parse();
