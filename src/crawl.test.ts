9248920a390ae328d9bc462608c49972e393021a
pmoeller91 <pmoeller91@gmail.com>
2024-07-08 14:54:51 -0500

    feat: add getURLsFromHTML

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "@jest/globals";
-import { normalizeURL } from "./crawl.ts";
+import { getURLsFromHTML, normalizeURL } from "./crawl.ts";
 
 describe("normalizeURL", () => {
   it.each(["https", "http", "ftp", "smtp", "nonsense"])(
@@ -28,5 +28,100 @@ describe("normalizeURL", () => {
     const expectedUrl = "google.com";
     const normalizedUrl = normalizeURL(url);
     expect(normalizedUrl).toEqual(expectedUrl);
+  });
+});
+
+describe("getURLsFromHTML", () => {
+  it("should extract a single link", () => {
+    const document = `
+    <html>
+      <body>
+          <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+      </body>
+    </html>
+    `;
+    const expectedLinks = ["https://blog.boot.dev/"];
+    const links = getURLsFromHTML(document, "https://google.com");
+    expect(links).toEqual(expectedLinks);
+  });
+  it("should extract an absolute link based on the base page, when an href has a relative link", () => {
+    const document = `
+    <html>
+      <body>
+          <a href="top/secret"><span>Go to Boot.dev</span></a>
+      </body>
+    </html>
+    `;
+    const expectedLinks = ["https://google.com/top/secret"];
+    const links = getURLsFromHTML(document, "https://google.com");
+    expect(links).toEqual(expectedLinks);
+  });
+  it("should extract multiple links", () => {
+    const document = `
+    <html>
+      <body>
+          <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+          <a href="https://other-blog.boot.dev"><span>Go to Other Boot.dev</span></a>
+          <a href="https://other-other-blog.boot.dev"><span>Go to Other Other Boot.dev</span></a>
+      </body>
+    </html>
+    `;
+    const expectedLinks = [
+      "https://blog.boot.dev/",
+      "https://other-blog.boot.dev/",
+      "https://other-other-blog.boot.dev/",
+    ];
+    const links = getURLsFromHTML(document, "https://google.com");
+    expect(links).toEqual(expectedLinks);
+  });
+  it("should extract no links", () => {
+    const document = `
+    <html>
+      <body>
+        <span>Hello World!</span>
+      </body>
+    </html>
+    `;
+    const expectedLinks = [];
+    const links = getURLsFromHTML(document, "https://google.com");
+    expect(links).toEqual(expectedLinks);
+  });
+  it("should extract no links when an anchor element lacks an href", () => {
+    const document = `
+    <html>
+      <body>
+        <a id="top"><span>Hello World!</span></a>
+      </body>
+    </html>
+    `;
+    const expectedLinks = [];
+    const links = getURLsFromHTML(document, "https://google.com");
+    expect(links).toEqual(expectedLinks);
+  });
+  it("should extract a link to the base page when an anchor has '#top' or similar as an href", () => {
+    const document = `
+    <html>
+      <body>
+        <a id="top"><span>Hello World!</span></a>
+        <a href="#top"><span>Go to top</span></a>
+      </body>
+    </html>
+    `;
+    const expectedLinks = ["https://google.com/#top"];
+    const links = getURLsFromHTML(document, "https://google.com");
+    expect(links).toEqual(expectedLinks);
+  });
+  it("should extract a link to the base page when an anchor has '#' as an href", () => {
+    const document = `
+    <html>
+      <body>
+        <a href="#"><span>Home</span></a>
+        <a><span>Hello World!</span></a>
+      </body>
+    </html>
+    `;
+    const expectedLinks = ["https://google.com/#"];
+    const links = getURLsFromHTML(document, "https://google.com");
+    expect(links).toEqual(expectedLinks);
   });
 });
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,11 +1,26 @@
+import { JSDOM } from "jsdom";
+
 /**
  * Normalizes a URL by stripping the protocol, trailing slashes, and so on
- * @param urlString The URL to normalize
- * @returns The normalized URL
+ * @param urlString - The URL to normalize
+ * @returns - The normalized URL
  */
-const normalizeURL = (urlString: string) => {
+const normalizeURL = (urlString: string): string => {
   const url = new URL(urlString);
-  return `${url.host}${url.pathname.replace(/\/$/, '')}`;
+  return `${url.host}${url.pathname.replace(/\/$/, "")}`;
 };
 
-export { normalizeURL };
+/**
+ * Extracts the URLs from any anchor tag in a document.
+ * @param htmlBody - A string representing an HTML document
+ * @param baseURL - The base URL of the document, to resolve relative links
+ * @returns - An array of absolute URLs, as strings
+ */
+const getURLsFromHTML = (htmlBody: string, baseURL: string): string[] => {
+  const dom = new JSDOM(htmlBody, { url: baseURL });
+  const anchorElements = dom.window.document.querySelectorAll<HTMLAnchorElement>("a[href]");
+  const urls = Array.from(anchorElements, (anchorElement) => anchorElement.href);
+  return urls;
+};
+
+export { normalizeURL, getURLsFromHTML };
